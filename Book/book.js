71501f8a9bbcdf1400f0ea19b4abce02c432f774
CodeMorera <morerachristian@yahoo.com>
2025-02-11 23:47:00 -0500

    Finished the form elements and continued the logic for book.js

--- Book/book.js
+++ Book/book.js
@@ -1,11 +1,50 @@
-console.log("Hello");
 
 const myLibrary = [];
 
-function Book(name){
-    this.name = name;
+class Book{
+    constructor(title,author,pages,read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
-function addBookToLibrary(){
+document.getElementById('book-form').addEventListener('submit',function(e){
+    e.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').checked;
+    addBookToLibrary(title,author,pages,read);
+    this.reset();
+});
 
+function displayBooks(){
+    const bookList = document.getElementById('book-list');
+    bookList.innerHTML = '';
+
+    myLibrary.forEach((book, index) =>{
+        const bookDiv = document.createElement('div');
+        bookDiv.classList.add('book');
+        bookDiv.innerHTML =
+        `<h3>${book.title}</h3>
+            <p>Author: ${book.author}</p>
+            <p>Pages: ${book.pages}</p>
+            <p>${book.read ? 'Read' : 'Not Read'}</p>
+            <button onclick="removeBook(${index})">Remove</button>
+        `;
+        bookList.appendChild(bookDiv);
+    });
+}
+
+function addBookToLibrary(title,author,pages,read){
+    const newBook = new Book(title,author,pages,read);
+    myLibrary.push(newBook);
+    displayBooks();
+}
+
+function removeBook(index){
+    myLibrary.splice(index,1);
+    displayBooks();
 }
